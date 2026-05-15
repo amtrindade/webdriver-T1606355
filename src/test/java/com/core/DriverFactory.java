948ce3fb905a01333ecaf2201c0f888127a6055d
Antonio Trindade <atrindade@umov.me>
2026-05-14 21:57:56 -0300

    Adiciona GlobalProperty

--- src/test/java/com/core/DriverFactory.java
+++ src/test/java/com/core/DriverFactory.java
@@ -4,7 +4,10 @@ import java.time.Duration;
 
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,11 +17,43 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver() {
 		
+		String typeBrowser = GlobarProperty.getProperty("brownser");
+		
 		if (driver == null) {
 			
-			WebDriverManager.firefoxdriver().setup();			
-			driver = new FirefoxDriver();
+			if (typeBrowser.equals("firefox")) {
 			
+				WebDriverManager.firefoxdriver().setup();			
+				driver = new FirefoxDriver();
+			}
+			else if (typeBrowser.equals("firefox-headless")) {
+			    FirefoxOptions options = new FirefoxOptions();
+			    options.addArguments("--headless");
+			    options.addArguments("--window-size=1200x860");
+				
+				WebDriverManager.firefoxdriver().setup();			
+				driver = new FirefoxDriver(options);
+			}
+			else if (typeBrowser.equals("chrome")) {
+				
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				
+			}
+			else if (typeBrowser.equals("chrome-headless")) {
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+			    options.addArguments("--window-size=1200x860");
+				
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver(options);
+				
+			}
+			else {
+				//Gerar uma exception erro
+				System.out.println("Browser inválido");
+			}
+									
 			//Abrir o browser no monitor auxiliar
 			Point point = new Point(-1500, -150); 
 			driver.manage().window().setPosition(point);
